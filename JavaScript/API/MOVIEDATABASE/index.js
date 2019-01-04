7998315ff21f308e3484b69abf8617f07739631e
Mckalloway <nasir.t@icloud.com>
2019-01-04 17:30:27 +0100

    yep

--- JavaScript/API/MOVIEDATABASE/index.js
+++ JavaScript/API/MOVIEDATABASE/index.js
@@ -36,23 +36,48 @@ const url_img = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
 
 //fonction pour montrer les informations du film
 function showMovie() {
-    var data = $(this).data('id');//stocker l'id du film dans une variable, "this" designe un seul élément selectionner
+    var data = $(this).data('id');//stocker l'id du film dans une variable, "this" designe l'élément selectionner
     $.getJSON('https://api.themoviedb.org/3/movie/'+data+'?api_key=2ee2c5b569240ea2a2a879dd9c8a822c', showDetailMovie);
     console.log(data);
 }
 
+
+//fonction callBack de la fonction precedente
 function showDetailMovie(response) {
     console.log(response);
     $('#title').html(response.original_title);
     $('#image').html('<img src="'+url_img+response.poster_path+'">');
     $('#overview').html(response.overview);
+    var companies = $('#companies').append('<h2>Companies</h2>')
+	companies.append('<ul>');
+    
+    
+    for(var i = 0; i < response.production_companies.length; i++) {
+    
+    	getCompanyWebSite(response.production_companies[i].id)
+        
+    }
 }
 
 
 
-$(document).on('click','li',showMovie);
+$(document).on('click','li',showMovie);//permet de recuperer les "li" apres chargement de la page
 
-   
+
+
+
+function getCompanyWebSite(id) {
+
+	$.getJSON('https://api.themoviedb.org/3/company/'+id+'?api_key=2ee2c5b569240ea2a2a879dd9c8a822c', displayCompany);
+	
+}
+
+function displayCompany(response) {
+			
+	console.log(response);
+	$('#companies ul').append('<li><a href="'+response.homepage+'" id="'+response.id+'">'+response.name+'</li></a>');
     
+}
+
     
     
